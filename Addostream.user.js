0c91fa7b071aa51e84edc29f9bb75fa60b00fd8b
nomomo <opandymion@gmail.com>
2017-06-06 16:41:17 +0900

    Update Addostream.user.js
    멀티트위치 추가, 스트리머 아이디 목록 추가

--- Addostream.user.js
+++ Addostream.user.js
@@ -3,7 +3,7 @@
 // @namespace   Addostream
 // @description 두스트림에 기능을 추가한다.
 // @include     http://*.dostream.com/*
-// @version     1
+// @version     1.01
 // @grant       none
 // ==/UserScript==
 
@@ -37,13 +37,23 @@ var streamerArray = [
     ['kss7749','쉐리'],
     ['looksam','룩삼'],
     ['saddummy','서새봄냥'],
-    ['109ace','철면수심']
+    ['109ace','철면수심'],
+    ['rhdgurwns','공혁준'],
+    ['gmdkdsla','흐앙님'],
+    ['jungtaejune','똘똘똘이'],
+    ['mascahs','마스카'],
+    ['steelohs','스틸로'],
+    ['kimdoe','김도'],
+    ['togom','토곰'],
+    ['ogn_lol','OGN 롤챔스']    
     ];
-var ADD_title = 'Twitch ID Checker v170527_002';
+var ADD_title = 'Twitch ID Checker v.17_0606';
 var href = 'initialize';
 var streamerID = '';
+var multitwitchID = 'hanryang1125';
 
-$('.container').append('<div style="height=63px;padding:30px 0;font-size:11px;font-style:italic;color:#999;">'+ADD_title+'</div>');
+$(document).on("click", "#multitwitch", multitwitch_run);
+$('.container').append('<div style="height=63px;padding:25px 0;font-size:11px;font-style:italic;color:#999;">'+ADD_title+'</div>');
 
 $(window).load(function(){
     Addostram_run();
@@ -60,6 +70,7 @@ $('.container>a').click(function(){
 
 function Addostram_run()
 {
+    $('.search').append('<span id="multitwitch" style="cursor: pointer; display:inline-block; font-size:12px; line-height:20px; margin:0 5px 0 0; padding: 5px 10px; background: #eee none repeat scroll 0 0; color: #222;">멀티트위치</span>');
     $('li.twitch').each(function (i) {
     href = $(this).find('a').attr('href');
     href = href.replace('/#/stream/twitch/', '');
@@ -71,6 +82,23 @@ function Addostram_run()
         }
     }
     $(this).find('.info>.from').html(streamerID+'('+href+')');
+    $(this).append('<div style="position:relative;"><div style="position:absolute; top:-40px; right:75px; text-align:center; font-size:12px; z-index:1000000; margin:0;"><input style="margin-right:5px;border:none !important;" type="checkbox" name="chk" value="'+href+'" onfocus="this.blur()" /><div style="background-color:#eee; padding:5px 8px; height:15; display:inline;"><a href="/#/stream/multitwitch/'+href+'">With chat</a></div></div></div>');
     streamerID = ''; // reset
 });
+};
+
+function multitwitch_run()
+{
+    multitwitchID = '';
+    $("input[name=chk]:checked").each(function() {
+        if(multitwitchID=='')
+           multitwitchID = $(this).val();
+        else
+           multitwitchID = multitwitchID+'&'+$(this).val();
+    });
+    //alert(multitwitchID);
+    if(multitwitchID=='')
+        alert('Check the checkboxs to use multitwitch!');
+    else
+        $(location).attr('href','/#/stream/multitwitch/'+multitwitchID);
 }
