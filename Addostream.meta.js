142d0859558ea6bade40febca3da04b421723cf0
Nomomo <opandymion@gmail.com>
2020-06-22 22:44:19 +0900

    1.53.5

--- Addostream.meta.js
+++ Addostream.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Addostream
-// @version 1.53.4
+// @version 1.53.5
 // @author Nomo
 // @description 두스트림에 기능을 추가한다.
 // @supportURL https://github.com/nomomo/Addostream/issues
