52d2c068472b28a73735b38740f2f94d4801e537
Nomomo <opandymion@gmail.com>
2020-11-30 00:41:16 +0900

    1.54.0

--- src/general/event_hijacking.js
+++ src/general/event_hijacking.js
@@ -4,26 +4,26 @@ export function event_hijacking(){
 
     if(nomo_global.PAGE == nomo_const.C_EMBEDED_TWITCH){
 
-        if(ADD_config.twitch_control && ADD_config.twitch_frontPageMode){
-            const origFetch = unsafeWindow.fetch;
-            unsafeWindow.fetch = (url, init, ...args) => {
-                if (typeof url === "string") {
-                    if (url.includes("/access_token")) {
-                        url = url.replace("player_type=site", "player_type=embed");
-                    } else if (
-                        url.includes("/gql") &&
-                        init &&
-                        typeof init.body === "string" &&
-                        init.body.includes("PlaybackAccessToken")
-                    ) {
-                        const newBody = JSON.parse(init.body);
-                        newBody.variables.playerType = "embed";
-                        init.body = JSON.stringify(newBody);
-                    }
-                }
-                return origFetch(url, init, ...args);
-            };
-        }
+        // if(ADD_config.twitch_control && ADD_config.twitch_frontPageMode){
+        //     const origFetch = unsafeWindow.fetch;
+        //     unsafeWindow.fetch = (url, init, ...args) => {
+        //         if (typeof url === "string") {
+        //             if (url.includes("/access_token")) {
+        //                 url = url.replace("player_type=site", "player_type=embed");
+        //             } else if (
+        //                 url.includes("/gql") &&
+        //                 init &&
+        //                 typeof init.body === "string" &&
+        //                 init.body.includes("PlaybackAccessToken")
+        //             ) {
+        //                 const newBody = JSON.parse(init.body);
+        //                 newBody.variables.playerType = "embed";
+        //                 init.body = JSON.stringify(newBody);
+        //             }
+        //         }
+        //         return origFetch(url, init, ...args);
+        //     };
+        // }
 
         if(ADD_config.twitch_control && ADD_config.twitch_disable_visibilitychange){
             unsafeWindow._addEventListener = unsafeWindow.addEventListener;
